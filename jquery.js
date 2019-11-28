5b221f0c99fec4b6b587f43905bdc6cc326f197b
wuxiao <1291383707@qq.com>
2019-11-28 11:57:47 +0800

    end

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-window.jQuery = function(selectorOrArray){//接受一个选择器
+window.jQuery = function(selectorOrArray){//接受一个选择器或数组
     let elements
     if(typeof selectorOrArray === 'string'){
         elements = document.querySelectorAll(selectorOrArray)
@@ -6,18 +6,24 @@ window.jQuery = function(selectorOrArray){//接受一个选择器
         elements = selectorOrArray
     }
     return {//返回一个对象，可以被链式调用
+
         addClass(className){   
         for (let i= 0; i < elements.length; i++) {
             elements[i].classList.add(className)
         }
         return this //指代 addClass() 对象
         },
+        oldApi: selectorOrArray.oldApi,
         find(selector){
             let array = []
             for ( let i = 0; i < elements.length; i++){
                 array = array.concat(Array.from(elements[i].querySelectorAll(selector)))
             }
+            array.oldApi = this //this 指旧的api
             return jQuery(array)// jquery 传啥返回啥
+        },
+        end(){
+            return this.oldApi //this指 新api
         }
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-jQuery('.test').find('.child').addClass('red')
+const api1 = jQuery('.test')
+const api2 = api1.find('.child').addClass('red')
+const oldApi = api2.end().addClass('blue') // 想把 blue 加到test上，调用end() 结束
