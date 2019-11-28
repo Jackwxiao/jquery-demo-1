9352b046a0ca2103ef60031db74d1569015a56a4
wuxiao <1291383707@qq.com>
2019-11-28 14:02:04 +0800

    实现获取儿子

--- jquery.js
+++ jquery.js
@@ -6,7 +6,6 @@ window.jQuery = function(selectorOrArray){//接受一个选择器或数组
         elements = selectorOrArray
     }
     return {//返回一个对象，可以被链式调用
-
         addClass(className){   
         for (let i= 0; i < elements.length; i++) {
             elements[i].classList.add(className)
@@ -21,6 +20,33 @@ window.jQuery = function(selectorOrArray){//接受一个选择器或数组
             }
             array.oldApi = this //this 指旧的api
             return jQuery(array)// jquery 传啥返回啥
+        },
+        each(fn){
+            for(let i = 0; i < elements.length; i++ ){
+                fn.call(null, elements[i], i)
+            }
+            return this
+        },
+        parent(){
+            const array = []
+            this.each((node)=>{
+                if(array.indexOf(node.parentNode) === -1){
+                    array.push(node.parentNode)
+                }
+            })
+            return jQuery(array)
+        },
+        children(){
+            const array = []
+            this.each((node)=>{
+                if(array.indexOf(node.children) === -1){
+                    array.push(...node.children)//展开操作符
+                }
+            })
+            return jQuery(array)
+        },
+        print(){
+            console.log(elements)
         },
         end(){
             return this.oldApi //this指 新api
--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-const api1 = jQuery('.test')
-const api2 = api1.find('.child').addClass('red')
-const oldApi = api2.end().addClass('blue') // 想把 blue 加到test上，调用end() 结束
+jQuery('.test')
+    .children().print()
